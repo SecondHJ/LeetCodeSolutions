5c72e5469f35f38b44d54ea1b2144b294b28b565
huajun <skatersecond@gmail.com>
2017-07-13 16:17:09 +0800

    SingleNumber

--- src/easy/SingleNumber.java
+++ src/easy/SingleNumber.java
@@ -6,20 +6,22 @@ package easy;
  * Note:
  * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
  *
+ * 解决思路:
+ * 嵌套循环检查当前下标值是否在整个数组其他地方出现过,
+ * 若再次出现continue外层循环继续搜索下一个数, 若未再次出现直接返回这个数
+ *
  * Created by second on 2017/7/13.
  */
 public class SingleNumber {
 
     public int singleNumber(int[] nums) {
-        int bar = 0;
         loop:
         for (int i = 0; i < nums.length; i++){
             for (int j = 0; j < nums.length; j++)
                 if (j != i && nums[i] == nums[j]) continue loop;
-            bar = nums[i];
-            break;
+            return nums[i];
         }
-        return bar;
+        return 0;
     }
 
 }
