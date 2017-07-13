22191c6e10c44e7e568bcca50e914ab46640a0c7
huajun <skatersecond@gmail.com>
2017-07-13 16:22:43 +0800

    ContainsDuplicate

--- src/easy/ContainsDuplicate.java
+++ src/easy/ContainsDuplicate.java
@@ -8,19 +8,17 @@ import java.util.Set;
  * Your function should return true if any value appears at least twice in the array,
  * and it should return false if every element is distinct.
  *
+ * 解决思路:
+ * Set去重，比较前后尺寸。。
+ *
  * Created by second on 2017/7/13.
  */
 public class ContainsDuplicate {
 
     public boolean containsDuplicate(int[] nums) {
         Set set = new HashSet();
-        for(int i : nums){
-            set.add(i);
-        }
-        if(set.size() == nums.length){
-            return false;
-        }
-        return true;
+        for(int i : nums) set.add(i);
+        return !(set.size() == nums.length);
     }
 
 }
