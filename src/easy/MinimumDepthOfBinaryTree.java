2e6f23c3f3a896a5fba5b2a4a2de6b053662a75f
huajun <skatersecond@gmail.com>
2017-07-25 22:09:56 +0800

    MinimumDepthOfBinaryTree

---
+++ src/easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+package easy;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ *
+ *
+ *
+ * Created by second on 2017/7/25.
+ */
+public class MinimumDepthOfBinaryTree {
+}
