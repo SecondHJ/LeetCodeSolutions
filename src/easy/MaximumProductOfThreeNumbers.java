4cf7b19edbc78f3e6c455fea586b62ad0d2f9257
huajun <skatersecond@gmail.com>
2017-09-05 21:21:16 +0800

    MaximumProductOfThreeNumbers

--- src/easy/MaximumProductOfThreeNumbers.java
+++ src/easy/MaximumProductOfThreeNumbers.java
@@ -4,9 +4,11 @@ import java.util.Arrays;
 
 /**
  * Given an integer array, find three numbers whose product is maximum and output the maximum product.
+ *
  * Example 1:
  * Input: [1,2,3]
  * Output: 6
+ *
  * Example 2:
  * Input: [1,2,3,4]
  * Output: 24
