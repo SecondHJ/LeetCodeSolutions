a6080051d90a7538690ebd44e1d76ad64a413fde
second <skatersecond@gmail.com>
2017-09-08 09:56:09 +0800

    MergeTwoBinaryTrees

--- src/easy/MergeTwoBinaryTrees.java
+++ src/easy/MergeTwoBinaryTrees.java
@@ -8,7 +8,7 @@ package easy;
  *
  * Example 1:
  * Input:
- * 	        Tree 1                     Tree 2
+ *        Tree 1                     Tree 2
  *           1                         2
  *          / \                       / \
  *         3   2                     1   3
