3284b56bc01674e77c6d4abf653c45591d1418f8
huajun <skatersecond@gmail.com>
2017-09-09 14:11:18 +0800

    TwoSum

--- src/easy/TwoSum.java
+++ src/easy/TwoSum.java
@@ -17,10 +17,9 @@ package easy;
 public class TwoSum {
 
     public int[] twoSum(int[] nums, int target) {
-        int[] copy = nums;
         for(int i = 0; i < nums.length; i++){
             for(int j = i + 1; j < nums.length; j++){
-                if((nums[i] + copy[j]) == target && i != j){
+                if((nums[i] + nums[j]) == target && i != j){
                     return new int[]{i,j};
                 }
             }
