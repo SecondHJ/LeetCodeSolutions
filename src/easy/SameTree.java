fa8ae4cad59c6ec2a55d6f0f5d85a3cb5469b6b6
huajun <skatersecond@gmail.com>
2017-09-16 12:02:54 +0800

    SameTree

--- src/easy/SameTree.java
+++ src/easy/SameTree.java
@@ -17,14 +17,10 @@ public class SameTree {
 
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if (p == null || q == null) return p == q;
-        if (p.val != q.val){
-            return false;
-        } else{
-            boolean left = isSameTree(p.left, q.left);
-            boolean right = isSameTree(p.right, q.right);
-            if(!(left && right)) return false;
-        }
-        return true;
+        if (p.val != q.val) return false;
+        boolean left = isSameTree(p.left, q.left);
+        boolean right = isSameTree(p.right, q.right);
+        return left && right;
     }
 
     private class TreeNode{
