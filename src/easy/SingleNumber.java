0100c5405e6b61c8615a742853dd535ee9dafee3
second <skatersecond@gmail.com>
2018-08-23 16:03:45 +0800

    Single Number

--- src/easy/SingleNumber.java
+++ src/easy/SingleNumber.java
@@ -10,6 +10,13 @@ package easy;
  * 嵌套循环检查当前下标值是否在整个数组其他地方出现过,
  * 若再次出现continue外层循环继续搜索下一个数, 若未再次出现直接返回这个数
  *
+ * 解法2 （极其优雅）
+ * 用或非(XOR)运算
+ * 因为
+ * 0 ^ a = a
+ * a ^ a = 0
+ * a ^ b ^ a = (a ^ a) ^ b = b
+ *
  * Created by second on 2017/7/13.
  */
 public class SingleNumber {
@@ -23,5 +30,14 @@ public class SingleNumber {
         }
         return 0;
     }
+
+    public int singleNumber2(int[] nums) {
+        int a = 0;
+        for(int i: nums){
+            a ^= i;
+        }
+        return a;
+    }
+
 
 }
